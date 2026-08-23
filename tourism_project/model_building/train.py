1c3eeca85a08c7e9e475ca2ae7c26b086c0fd117
aditisaxena-ai <aditis2508@gmail.com>
2026-08-23 19:51:04 +0530

    add tourism_project/model_building/train.py

---
+++ tourism_project/model_building/train.py
@@ -0,0 +1,9 @@
+
+import pandas as pd from sklearn.model_selection 
+import GridSearchCV from sklearn.metrics 
+import classification_report 
+import joblib 
+import mlflow from sklearn.preprocessing 
+import StandardScaler, OneHotEncoder from sklearn.compose 
+import make_column_transformer from sklearn.pipeline 
+import make_pipeline import xgboost as xgb import os
